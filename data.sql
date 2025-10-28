7a228aaa6f60f8b21dd57c55d945acceea4d8fe9
juiyzzone97-스택 <juoyzzone97@gmail.com>
2025-10-28 16:33:09 +0900

    주문하기 추가

--- data.sql
+++ data.sql
@@ -36,6 +36,7 @@ JOIN category ON books.category_id = category_id;
 
 SELECT * FROM books LEFT JOIN category ON books.category_id = category_id WHERE books.id=1;
 
+-- 좋아요 추가
 INSERT INTO likes (user_id, liked_book_id) VALUES (1, 1);
 INSERT INTO likes (user_id, liked_book_id) VALUES (1, 2);
 INSERT INTO likes (user_id, liked_book_id) VALUES (1, 3);
@@ -45,8 +46,44 @@ INSERT INTO likes (user_id, liked_book_id) VALUES (2, 1);
 INSERT INTO likes (user_id, liked_book_id) VALUES (2, 2);
 INSERT INTO likes (user_id, liked_book_id) VALUES (2, 3);
 INSERT INTO likes (user_id, liked_book_id) VALUES (2, 5);
+
+-- 좋아요 삭제
 DELETE FROM likes WHERE user_id = 1 AND liked_book_id = 3;
+
+
 
 SELECT *,
 	(SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes
     FROM books;
+
+INSERT INTO cartItems (book_id, quantity, user_id) VALUES (1, 1, 1);
+
+-- 장바구니 조회
+SELECT cartitems.id, book_id, title, summary, quantity, price FROM cartitems LEFT JOIN books ON cartitems.book_id = books.id;
+
+//장바구니 아이템 조회
+DELETE FROM cartitems WHERE id = ?;
+
+//장바구니에서 선택한(장바구니 도서 id) 아이템 목록 조회 = 선택한 장바구니 상품 목록 조회
+SELECT *FROM Bookshop.cartitems WHERE user_id=1 AND id IN (1,3)
+
+//주문하기
+// 배송 정보 입력
+INSERT INTO delivery (address, receiver, contact) VALUES ("서울시 서초구", "오하하", "010-5555-5555")
+const delivery_id = SELECT max(id) FROM delivery;
+
+//주문 정보 입력
+INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+VALUES ("어른왕자들", 3, 60000, 1, delivery_id)
+const order_id = SELECT max(id) FROM orders;
+//주문 상세 목록 입력
+INSERT INTO orderedBook (order_id, book_id, quantity)
+VALUES (order_id, 1, 1);
+INSERT INTO orderedBook (order_id, book_id, quantity)
+VALUES (order_id, 3, 2);
+
+SELECT max(id) FROM BookShop.orderedBook;
+SELECT last_insert_id();
+
+// 결제된 도서 장바구니 삭제
+DELETE FROM cartitems WHERE id IN (1,2,3)
--- mariadb.js
+++ mariadb.js
@@ -1,4 +1,4 @@
-const mariadb =require('mysql2');
+const mariadb = require('mysql2');
 
 const connection = mariadb.createConnection({
   host : 'localhost',
